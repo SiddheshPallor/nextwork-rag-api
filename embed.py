4d555f3a38bffecf1ea3f166ba6925fc2ca3b925
SiddheshPallor <sidhesh2508@gmail.com>
2026-03-14 08:29:43 +0530

    Secret mission: multiple docs support

--- embed.py
+++ embed.py
@@ -1,11 +1,22 @@
 import chromadb
+import os
 
 client = chromadb.PersistentClient(path="./db")
 collection = client.get_or_create_collection("docs")
 
-with open("k8s.txt", "r") as f:
-    text = f.read()
+docs_folder = "docs"
 
-collection.add(documents=[text], ids=["k8s"])
+for file in os.listdir(docs_folder):
+    if file.endswith(".txt"):
+
+        with open(f"{docs_folder}/{file}", "r") as f:
+            text = f.read()
+
+        collection.add(
+            documents=[text],
+            ids=[file]
+        )
+
+        print(file + " embedded")
 
 print("Embedding stored in Chroma")
